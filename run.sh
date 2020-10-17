eaa52d7fd89a4bf3f94efa67bb72fec909ac740d
Neeraj Kashyap <nkashy1@gmail.com>
2020-10-17 11:25:26 -0700

    Trying to figure out how to set defaults
    Using GH environment variables.
    
    Inspecting event payloads

--- run.sh
+++ run.sh
@@ -7,5 +7,16 @@ TERMINAL_REF="$INPUT_TERMINAL_REF"
 REPO="/github/workspace/$REPO_SUBDIR"
 
 echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"
+echo "GITHUB_EVENT:"
+cat "$GITHUB_EVENT_PATH"
+
+if [ -z "$INITIAL_REF" ]
+then
+    if [ "$GITHUB_EVENT_NAME" = "push" ]
+    then
+        INITIAL_REF="HEAD~1"
+    fi
+fi
+
 
 locust --format yaml -r "$REPO" $INITIAL_REF $TERMINAL_REF
