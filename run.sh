721aa8603fa627f05286897719d59d9d151cb94e
Neeraj Kashyap <nkashy1@gmail.com>
2020-10-17 11:21:38 -0700

    Show GITHUB_EVENT_NAME

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
-set -ex
+set -e
 
 INITIAL_REF="$INPUT_INITIAL_REF"
 TERMINAL_REF="$INPUT_TERMINAL_REF"
 REPO="/github/workspace/$REPO_SUBDIR"
+
+echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"
 
 locust --format yaml -r "$REPO" $INITIAL_REF $TERMINAL_REF
