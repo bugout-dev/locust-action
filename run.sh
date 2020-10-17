320f8d58c3e09f763e68c206f76cccc7e5cd1752
Neeraj Kashyap <nkashy1@gmail.com>
2020-10-17 12:03:32 -0700

    Modified run.sh to use the new locust GHA helpers

--- run.sh
+++ run.sh
@@ -6,17 +6,14 @@ INITIAL_REF="$INPUT_INITIAL_REF"
 TERMINAL_REF="$INPUT_TERMINAL_REF"
 REPO="/github/workspace/$REPO_SUBDIR"
 
-echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"
-echo "GITHUB_EVENT:"
-cat "$GITHUB_EVENT_PATH"
-
 if [ -z "$INITIAL_REF" ]
 then
-    if [ "$GITHUB_EVENT_NAME" = "push" ]
-    then
-        INITIAL_REF="HEAD~1"
-    fi
+    INITIAL_REF=$(locust.github initial)
 fi
 
+if [ -z "$TERMINAL_REF" ]
+then
+    TERMINAL_REF=$(locust.github terminal)
+fi
 
-locust --format yaml -r "$REPO" $INITIAL_REF $TERMINAL_REF
+locust --format yaml -r "$REPO" "$INITIAL_REF" "$TERMINAL_REF"
