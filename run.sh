5bc192943efc9294bd99fc7685939353557d158c
Sergei Sumarokov <zenitsws@gmail.com>
2020-11-11 04:28:42 -0800

    Removed if

--- run.sh
+++ run.sh
@@ -18,14 +18,9 @@ then
 fi
 
 REPO_URL=$(locust.github repo)
+COMMENTS_URL=$(python -c 'import json; import os; event = os.environ.get("GITHUB_EVENT_PATH"); raw = open(event); inp_json = json.load(raw); print(inp_json.get("pull_request").get("_links").get("comments").get("href")); raw.close();')
 
-if [ "$BUGOUT_SEND_SUMMARY" = true ]
-then
-    COMMENTS_URL=$(python -c 'import json; import os; event = os.environ.get("GITHUB_EVENT_PATH"); raw = open(event); inp_json = json.load(raw); print(inp_json.get("pull_request").get("_links").get("comments").get("href")); raw.close();')
-    locust --format "$FORMAT" -r "$REPO" "$INITIAL_REF" "$TERMINAL_REF" --github "${REPO_URL}" --metadata "{\"comments_url\": \"${COMMENTS_URL}\", \"terminal_hash\": \"$TERMINAL_REF\"}" | tee /locust.summary
-else
-    locust --format "$FORMAT" -r "$REPO" "$INITIAL_REF" "$TERMINAL_REF" --github "${REPO_URL}" | tee /locust.summary
-fi
+locust --format "$FORMAT" -r "$REPO" "$INITIAL_REF" "$TERMINAL_REF" --github "${REPO_URL}" --metadata "{\"comments_url\": \"${COMMENTS_URL}\", \"terminal_hash\": \"$TERMINAL_REF\"}" | tee /locust.summary
 
 # Thanks to this GitHub community post:
 # https://github.community/t/set-output-truncates-multiline-strings/16852/3
